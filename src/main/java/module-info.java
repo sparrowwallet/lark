e41ca0a5a8de91c2c1ec37f3f5b4404748bccde9
Craig Raw <craigraw@gmail.com>
2024-11-25 17:36:48 +0200

    add module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.sparrowwallet.lark {
+    requires co.nstant.in.cbor;
+    requires com.fazecast.jSerialComm;
+    requires com.google.protobuf;
+    requires com.sparrowwallet.drongo;
+    requires com.sparrowwallet.tern;
+    requires org.hid4java;
+    requires com.google.code.findbugs.jsr305;
+    requires org.apache.commons.codec;
+    requires org.slf4j;
+    requires org.usb4java;
+    requires com.fasterxml.jackson.databind;
+    requires com.google.common;
+    exports com.sparrowwallet.lark;
+}
